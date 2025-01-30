6cd6b408534590d654064e272678c2515f75e428
Yohana Villarraga <nyvillarraga@gmail.com>
2025-01-30 00:56:36 -0500

    act

--- src/main/java/com/proyectoprueba/cafebar/config/webcConfig.java
+++ src/main/java/com/proyectoprueba/cafebar/config/webcConfig.java
@@ -11,10 +11,10 @@ public class webcConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         // Permite todas las rutas y orígenes
-        registry.addMapping("/**") // Permite solicitudes a todas las rutas
-            .allowedOrigins("http://127.0.0.1:5500") // Tu origen de frontend
-            .allowedMethods("GET", "POST", "DELETE", "PUT", "OPTIONS") // Métodos permitidos
-            .allowedHeaders("*") // Todos los headers permitidos
+        registry.addMapping("/**") // Aca permite solicitudes a todas las rutas
+            .allowedOrigins("http://127.0.0.1:5500") // Aca tu origen de frontend
+            .allowedMethods("GET", "POST", "DELETE", "PUT", "OPTIONS") // Aca los métodos permitidos
+            .allowedHeaders("*") // Aca todos los headers permitidos
             .allowCredentials(true); // Permite credenciales (si es necesario)
     }
 }
